00bd273ea815478ca39192cd379eaca9ed0e5f63
guilherme Silva Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2022-08-17 14:22:34 -0300

    move geraSigla
    move o método gera sigla para a classe que utilizará ela

--- src/sapo/Atividade.java
+++ src/sapo/Atividade.java
@@ -11,29 +11,12 @@ public class Atividade {
 	private int tafetasCadastradas;
 	private Map<String,Tarefas> listaTarefas;
 	
-	public Atividade(String nome,String decricao,String cpfResponsavel) {
+	public Atividade(String nome,String decricao,String cpfResponsavel,String sigla) {
 		this.nome = nome;
 		this.decricao = decricao;
 		this.cpfResponsavel = cpfResponsavel;
-		this.codigo = this.geraSigla(nome);
-	}
-	
-	private String geraSigla(String nome) {
-		String sigla = "";
-		String vogais = "aeiouAEIOU ";
-		for(int i =0;i<nome.length();i++) {
-			String letra = "" + nome.charAt(i);
-			if (codigo.length() < 3 || !vogais.contains(letra)) {
-				sigla += letra;
-			}
-			}
-		while (codigo.length() < 3){
-			sigla += "X";
-		}
-		return sigla;
-	}
-	
-	
+		this.codigo = sigla;
+	}	
 	
 	@Override
 	public String toString() {
--- src/sapo/AtividadeRepository.java
+++ src/sapo/AtividadeRepository.java
@@ -12,5 +12,21 @@ public class AtividadeRepository {
 	}
 	public void alterarDescricaoAtividade(String atividadeId,String descricao) {return;}
 	public void alterarResponsavelAtividade(String atividadeId,String cpf) {return;}
+	
+	private String geraSigla(String nome) {
+		String sigla = "";
+		String vogais = "aeiouAEIOU ";
+		for(int i =0;i<nome.length();i++) {
+			String letra = "" + nome.charAt(i);
+			if (sigla.length() < 3 || !vogais.contains(letra)) {
+				sigla += letra;
+			}
+			}
+		while (sigla.length() < 3){
+			sigla += "X";
+		}
+		return sigla;
+	}
+	
 
 }

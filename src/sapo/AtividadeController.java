8c7ec4ff3a05c58a434deefb7dfd5e0810fd9b84
guilherme Silva Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2022-08-17 23:16:51 -0300

    chama AtividadeRepository

--- src/sapo/AtividadeController.java
+++ src/sapo/AtividadeController.java
@@ -2,13 +2,37 @@ package sapo;
 
 public class AtividadeController {
 	AtividadeRepository ar;
-	public String cadastrarAtividade(String nome,String descricao,String cpf) {return null;}
-	public void encerrarAtividade(String atividadeId) {return;}
-	public void desativarAtividade(String atividadeId) {return;}
-	public void reabrirAtividade(String atividadeId) {return;}
-	public String exibirAtividade(String atividadeId) {return null;}
-	public void alterarDescricaoAtividade(String atividadeId,String descricao) {return;}
-	public void alterarResponsavelAtividade(String atividadeId,String cpf) {return;}
+	public AtividadeController(AtividadeRepository ar){
+		this.ar = ar;
+	}
+	
+	public String cadastrarAtividade(String nome,String descricao,String cpf) {
+		return ar.cadastrarAtividade(nome, descricao, cpf);
+	}
+	
+	public void encerrarAtividade(String atividadeId) {
+		ar.encerrarAtividade(atividadeId);
+	}
+	
+	public void desativarAtividade(String atividadeId) {
+		ar.desativarAtividade(atividadeId);
+	}
+	
+	public void reabrirAtividade(String atividadeId) {
+		ar.reabrirAtividade(atividadeId);
+	}
+	
+	public String exibirAtividade(String atividadeId) {
+		return ar.exibirAtividade(atividadeId);
+	}
+	
+	public void alterarDescricaoAtividade(String atividadeId,String descricao) {
+		ar.alterarDescricaoAtividade(atividadeId, descricao);
+	}
+	
+	public void alterarResponsavelAtividade(String atividadeId,String cpf) {
+		ar.alterarDescricaoAtividade(atividadeId, cpf);
+	}
 
 
 }

17667719a43410ed79b8f7f8c51595fa7ab79ae3
guilherme Silva Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2022-08-17 23:31:52 -0300

    logica para fechar encerrar e reabrir atividade

--- src/sapo/Atividade.java
+++ src/sapo/Atividade.java
@@ -7,17 +7,56 @@ public class Atividade {
 	private String codigo;
 	private String decricao;
 	private String cpfResponsavel;
+	private String status;
 	private int tarefasRealizadas;
+	private int tarefasPendentes;
 	private int tafetasCadastradas;
-	private Map<String,Tarefas> listaTarefas;
-	
-	public Atividade(String nome,String decricao,String cpfResponsavel,String sigla) {
+	private Map<String, Tarefas> listaTarefas;
+
+	public Atividade(String nome, String decricao, String cpfResponsavel, String sigla) {
 		this.nome = nome;
 		this.decricao = decricao;
 		this.cpfResponsavel = cpfResponsavel;
 		this.codigo = sigla;
-	}	
-	
+		this.status = "aberta";
+		this.tafetasCadastradas = 0;
+		this.tarefasPendentes = 0;
+		this.tarefasRealizadas = 0;
+	}
+
+	public void encerrar() throws IllegalStateException {
+		if (this.status == "aberta") {
+			if (this.tarefasPendentes == 0) {
+				this.status = "encerrada";
+			} else {
+				throw new IllegalStateException("tarefas ainda pendentes");
+			}
+		} else {
+			throw new IllegalStateException("atividade nao aberta");
+		}
+	}
+
+	public void desativar() {
+		if (this.status == "aberta") {
+			if (this.tarefasPendentes == 0) {
+				this.status = "desativada";
+			} else {
+				throw new IllegalStateException("tarefas ainda pendentes");
+			}
+		} else {
+			throw new IllegalStateException("atividade nao aberta");
+		}
+	}
+
+	public void reabrir() {
+		if (this.status == "aberta") {
+			throw new IllegalStateException("atividade ja aberta");
+		} else {
+			this.status = "aberta";
+			
+		}
+	}
+
 	@Override
 	public String toString() {
 		return null;

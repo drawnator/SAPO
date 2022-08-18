d1c92c4dc79d05f6ddfeec51726fa8a79c222d5d
guilherme Silva Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2022-08-17 23:08:40 -0300

    setup AtividadeController

--- src/sapo/AtividadeController.java
+++ src/sapo/AtividadeController.java
@@ -1,5 +1,14 @@
 package sapo;
 
 public class AtividadeController {
+	AtividadeRepository ar;
+	public String cadastrarAtividade(String nome,String descricao,String cpf) {return null;}
+	public void encerrarAtividade(String atividadeId) {return;}
+	public void desativarAtividade(String atividadeId) {return;}
+	public void reabrirAtividade(String atividadeId) {return;}
+	public String exibirAtividade(String atividadeId) {return null;}
+	public void alterarDescricaoAtividade(String atividadeId,String descricao) {return;}
+	public void alterarResponsavelAtividade(String atividadeId,String cpf) {return;}
+
 
 }

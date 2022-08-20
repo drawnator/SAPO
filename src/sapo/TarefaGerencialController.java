b55f436c29922d688970e9c627feddc39f68c084
Erick Farias de Almeida Pereira <erick.pereira@ccc.ufcg.edu.br>
2022-08-20 20:04:09 -0300

    Update TarefaGerencialController.java

--- src/sapo/TarefaGerencialController.java
+++ src/sapo/TarefaGerencialController.java
@@ -1,5 +1,25 @@
 package sapo;
 
 public class TarefaGerencialController {
-
+	
+	TarefaGerencialService tgs;
+	
+	public TarefaGerencialController(TarefaGerencialService tgs) {
+		this.tgs = tgs;
+	}
+	
+	public String cadastrarTarefaGerencial(String atividadeId, String nome, String[] habilidades, String[] idTarefas) {
+		
+	}
+	
+	public void adicionarNaTarefaGerencial(String idTarefaGerencial, String idTarefa) {
+		
+	}
+	
+	public void removerDaTarefaGerencial(String idTarefaGerencial, String idTarefa) {
+		
+	}
+	public int contarTodasTarefasNaTarefaGerencial(String idTarefaGerencial) {
+		
+	}
 }

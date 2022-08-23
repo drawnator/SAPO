cd0919541171ee6eba6425ad856484d6ef299591
guilherme Silva Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2022-08-23 10:22:09 -0300

    retira acentos
    para evitar problemas de renderização no eclipse

--- src/sapo/pessoa/PessoaService.java
+++ src/sapo/pessoa/PessoaService.java
@@ -13,10 +13,10 @@ public class PessoaService {
 	
 	public void cadastrarPessoa(String cpf, String nome, String[] habilidades) throws IllegalArgumentException{
 		if (cpf.equals("")) {
-			throw new IllegalArgumentException("cpf inválido");
+			throw new IllegalArgumentException("cpf invalido");
 		}
 		else if (nome.equals("")) {
-			throw new IllegalArgumentException("nome inválido");
+			throw new IllegalArgumentException("nome invalido");
 		}
 		Pessoa novaPessoa = new Pessoa(cpf, nome, habilidades);
 		pr.cadastraPessoa(cpf, novaPessoa);
@@ -28,7 +28,7 @@ public class PessoaService {
 	
 	public void alterarNomePessoa(String cpf, String novoNome) {
 		if (novoNome.equals("")) {
-			throw new IllegalArgumentException("nome inválido");
+			throw new IllegalArgumentException("nome invalido");
 		}
 		pr.getPessoa(cpf).setNome(novoNome);
 	}

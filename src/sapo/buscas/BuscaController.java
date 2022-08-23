9dc94c8903c6f8078b5ff75cd438f156da4451d8
guilherme Silva Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2022-08-22 22:01:19 -0300

    setup metodos

--- src/sapo/buscas/BuscaController.java
+++ src/sapo/buscas/BuscaController.java
@@ -2,4 +2,17 @@ package sapo.buscas;
 
 public class BuscaController {
 
+	public String[] exibirPessoas(String consulta) {return null;}
+
+	public String[] buscarAtividade(String consulta){return null;}
+
+	public String[] buscarTarefas(String nome){return null;}
+
+	public String[] buscarTarefas(String idAtividade,String nome){return null;}
+
+	public String[] sugerirTarefas(String idAtividade,String cpf){return null;}
+
+	public String[] buscasMaisRecentes(int nBuscas) {return null;}
+
+	public String[] exibirHistóricoBusca(int indexBusca) {return null;}
 }

e33f8b5eddd4011887f5e230fbd341781de9e6ef
guilherme Silva Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2022-08-23 10:56:27 -0300

    implementacao de historico

--- src/sapo/buscas/Busca.java
+++ src/sapo/buscas/Busca.java
@@ -1,5 +1,9 @@
 package sapo.buscas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 import sapo.atividade.AtividadeService;
 import sapo.pessoa.PessoaService;
 import sapo.tarefa.TarefaService;
@@ -8,25 +12,57 @@ public class Busca {
 	AtividadeService as;
 	PessoaService ps;
 	TarefaService ts;
+	ArrayList<String[]> historicoBusca;
 
-	public Busca(AtividadeService as,PessoaService ps, TarefaService ts) {
+	public Busca(AtividadeService as, PessoaService ps, TarefaService ts) {
 		this.as = as;
 		this.ps = ps;
 		this.ts = ts;
-		
+
 	}
 
-	public String[] exibirPessoas(String consulta) {return null;}
+	public String[] exibirPessoas(String consulta) {
+		String[] resultado = (String[]) ps.busca(consulta).toArray();
+		String[] tipo = {"PESSOA"};
+		addHistorico(tipo,resultado);
+		return resultado;
+	}
 
-	public String[] buscarAtividade(String consulta){return null;}
+	public String[] buscarAtividade(String consulta) {
+		String[] resultado = (String[]) as.busca(consulta).toArray();
+		String[] tipo = {"ATIVIDADE"};
+		addHistorico(tipo,resultado);
+		return resultado;
+	}
 
-	public String[] buscarTarefas(String nome){return null;}
+	public String[] buscarTarefas(String nome) {
+		String[] resultado = (String[]) ts.busca(nome).toArray();
+		String[] tipo = {"TAREFA"};
+		addHistorico(tipo,resultado);
+		return resultado;
+	}
 
-	public String[] buscarTarefas(String idAtividade,String nome){return null;}
+	public String[] buscarTarefas(String idAtividade, String nome) {
+		String[] resultado = (String[]) as.busca(idAtividade,nome).toArray();
+		String[] tipo = {"TAREFA"};
+		addHistorico(tipo,resultado);
+		return resultado;
+	}
 
-	public String[] sugerirTarefas(String idAtividade,String cpf){return null;}
+	public String[] sugerirTarefas(String idAtividade, String cpf) {
+		return null;
+	}
 
-	public String[] buscasMaisRecentes(int nBuscas) {return null;}
+	public String[] buscasMaisRecentes(int nBuscas) {
+		return null;
+	}
 
-	public String[] exibirHistóricoBusca(int indexBusca) {return null;}
+	public String[] exibirHistoricoBusca(int indexBusca) {
+		return null;
+	}
+	
+	private void addHistorico(String[] tipo, String[] resultado) {
+		historicoBusca.add((String[]) Stream.concat(
+				Arrays.stream(tipo), Arrays.stream(resultado)).toArray());
+	}
 }
